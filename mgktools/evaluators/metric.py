39556dd5f7634aea1e1c186273d045f2f55d3cf4
xiangyan93 <yan.xiang@duke.edu>
2024-05-24 00:11:46 -0400

    V2.1.2: Bugfix for notebooks. update for RMSE.

--- mgktools/evaluators/metric.py
+++ mgktools/evaluators/metric.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy
 from sklearn.metrics import (
     mean_squared_error,
+    root_mean_squared_error,
     mean_absolute_error,
     r2_score,
     accuracy_score,
@@ -49,7 +50,7 @@ def eval_metric_func(y: List[float], y_pred: List[float], metric: Metric) -> flo
     elif metric == 'mse':
         return mean_squared_error(y, y_pred)
     elif metric == 'rmse':
-        return mean_squared_error(y, y_pred, squared=False)
+        return root_mean_squared_error(y, y_pred)
     elif metric == 'max':
         return np.max(abs(y - y_pred))
     elif metric == 'spearman':
